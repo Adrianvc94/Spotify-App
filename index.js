49529c8b857fda44788a65dfc7232ceb7f761371
Adrianvc94 <avicamleon@gmail.com>
2021-10-06 07:24:41 -0600

    First part of authorization is complete   The app redirects to spotify so the user can login into his account

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
 require('dotenv').config();
 const express = require('express');
+const querystring = require('querystring');
 const app = express();
 const port = 8888;
 
-console.log(process.env.CLIENT_ID);
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
-app.get('/', (req, res) => {
-    const data = {
-        name: 'Just a name',
-        isAwesome: true
-    };
 
-    res.json(data);
+const generateRandomString = length => {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPGRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for(let i = 0; i < length; i++){
+        text += possible.charAt(Math.floor(Math.random()) * possible.length);
+    }
+
+    return text;
+}
+
+const statekey = 'spotify_auth_state';
+
+
+app.get('/login', (req, res) => {
+
+    const state = generateRandomString(16);
+
+    res.cookie(statekey, state);
+
+    const scope = 'user-read-private user-read-email';
+
+    const queryParams = querystring.stringify({
+        client_id : CLIENT_ID,
+        response_type : 'code',
+        redirect_uri : REDIRECT_URI,
+        state : state,
+        scope : scope
+    });
+    
+    res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
+
+
 
 
 
