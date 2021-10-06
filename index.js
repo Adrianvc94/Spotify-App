fee939223f41d2fd7808094c587084793b540854
Adrianvc94 <avicamleon@gmail.com>
2021-10-06 07:57:08 -0600

    The app now receives the access token to begin requesting for user data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const querystring = require('querystring');
+const axios = require('axios');
 const app = express();
 const port = 8888;
 
@@ -42,6 +43,35 @@ app.get('/login', (req, res) => {
     res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
+
+app.get('/callback', (req, res) => {
+    
+    const code = req.query.code || null;
+
+    axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: querystring.stringify({
+            grant_type: 'authorization_code',
+            code: code,
+            redirect_uri: REDIRECT_URI
+        }),
+        headers: {
+            'content_type': 'application/x-www-form-urlencoded',
+            Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`
+        }  
+    })
+    .then(response => {
+        if(response.status === 200){
+            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+        }else{
+            res.send(response);
+        }
+    })
+    .catch(error => {
+        res.send(error);
+    })
+});
 
 
 
